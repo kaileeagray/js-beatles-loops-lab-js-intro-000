f511bac44973f7212a84fdabf14c4f831d27caff
Kailee Gray <kaileeagray@gmail.com>
2019-05-02 17:06:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var beatlesArray = [];
   for (var i = 0, i < musicians.length, i++) {
-    beatlesArray[i] = `${musicians[i]} plays`
+    beatlesArray[i] = `${musicians[i]} plays ${instruments[i]}`
   }
 }
